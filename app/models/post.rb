592d2595d65ed14dbcda83e1696c1584b5c601e2
Sarah <sarahyun7@gmail.com>
2019-01-16 09:07:47 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 200 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    if title && title.match("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+      return true
+    else
+      errors.add(:clickbait, "Title is missing")
+    end
+  end
 end
